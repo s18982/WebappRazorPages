be634aa8b44d95bf91e270d7b93c5b22bf156d9c
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-07-14 11:29:41 +0200

    Add-migration

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
