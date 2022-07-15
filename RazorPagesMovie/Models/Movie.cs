e0c65e41cc8c96d1d7804e55b12dbd358eefd3e1
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-07-15 21:18:02 +0200

    Podlaczenie stron Movies; Poprawione kolumny Movie

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Castle.MicroKernel.SubSystems.Conversion;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+        [Display(Name ="Tytuł")]
         public string Title { get; set; } = string.Empty;
+        [Display(Name = "Data wydania")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Gatunek")]
         public string Genre { get; set; } = string.Empty;
+        [Display(Name = "Cena")]
+        [Column(TypeName="decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
