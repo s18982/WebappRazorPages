446dd0832c37ee284b108fb992187e9ed92b9ce8
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-07-18 22:58:46 +0200

    walidacja w modelu

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -11,16 +11,25 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
-        [Display(Name ="Tytuł")]
+        [Display(Name = "Tytuł")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; } = string.Empty;
         [Display(Name = "Data wydania")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         [Display(Name = "Gatunek")]
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(30)]
         public string Genre { get; set; } = string.Empty;
         [Display(Name = "Cena")]
-        [Column(TypeName="decimal(18,2)")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        [Required]
+        [StringLength(5)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string Rating { get; set; } = string.Empty;
 
     }
